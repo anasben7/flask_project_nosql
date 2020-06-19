f1436b043ec2c0de9821ada5169a85752c0699fd
anasben7 <anas.benabdellah7@gmail.com>
2020-06-19 21:12:11 +0100

    clean_google_scrapping

--- projet/scrapping/scraping.py
+++ projet/scrapping/scraping.py
@@ -12,50 +12,34 @@ def fetch_url(country):
     response = requests.get(url)
     return response.content
 
+
+def clean(descr):
+    descr = descr.replace('&quot;','')
+    descr = descr.replace('&#39;','')
+    descr = descr.replace('&nbsp;','')
+    return descr
+
+
 def fetch_trends(country):
     doc = fetch_url(country)
     soup = BeautifulSoup(doc, "html.parser")
 
     for item in soup.find_all("item"):
         title=item.title
-        print(title.text)
+        approx_traffic=item.contents[3]
+        description=item.contents[15]
+        trend_time=item.pubdate
+        db.trends.insert_one({"title":title.text,"approx_traffic":approx_traffic.text, "description":clean(description.contents[3].text), "trend_time":trend_time.text })
 
-
-        
-        description=soup.find('ht:news_item_snippe')
-        trend_time=soup.find('pubDate')
-        """ title=item.title
-        approx_traffic=item.approx_traffic
-        description=item.ht
-        trend_time=item.trend_time """
-    
-        
-        
-    """   approx_traffic=soup.find('ht:approx_traffic')
-        description=soup.find('ht:news_item_snippe')
-        trend_time=soup.find('pubDate') """   
-    """ descriptions=soup.find_all("description") """
     approximate_traffic = soup.find_all("ht:approx_traffic")
     titles = soup.find_all("title")
     
-    """ for title, traffic, description, trend_date in zip(titles[1:], approximate_traffic, descriptions[1:], trend_dates):
-        db.trends.insert_one({"title":title.text,"approx_traffic":traffic.text, "description":description.text, "trend_time":trend_date.text }) """
 
     
     return {title.text: re.sub("[+,]", "", traffic.text)
             for title, traffic in zip(titles[1:], approximate_traffic)}
 
-def fetch_trends2(country):
-    doc = fetch_url(country)
-    soup = BeautifulSoup(doc, "html.parser")
-    titles = soup.find_all("title")
-    descriptions=soup.find_all("description")
-    approximate_traffic = soup.find_all("ht:approx_traffic")
-    trend_dates = soup.find_all("pubdate")
 
-    
-    return {title.text: re.sub("[+,]", "", traffic.text)
-            for title, traffic in zip(titles[1:], approximate_traffic)}
 
 
 if __name__ == '__main__':
