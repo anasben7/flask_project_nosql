d066583b132430b4a9c8b6818378b0aea3cf87b0
anasben7 <anas.benabdellah7@gmail.com>
2020-06-21 14:48:54 +0100

    sunday_add_total_positivity

--- projet/backend/main.py
+++ projet/backend/main.py
@@ -4,11 +4,11 @@ import json
 from bson import json_util
 from flask import render_template
 import atexit
-from apscheduler.scheduler import Scheduler
+
 
 main= Blueprint('main',__name__)
 
-cron = Scheduler(daemon=True)
+
 
 
 @main.route('/')
